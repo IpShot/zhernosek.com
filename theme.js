26e26bfb95d537a4706382bfa4d97d5c3ddcb638
IpShot <zhernosek.roman@gmail.com>
2020-06-03 20:55:30 +0500

    update theme

--- theme.js
+++ theme.js
@@ -1,22 +1,25 @@
-import { createMuiTheme } from '@material-ui/core/styles';
-import { red } from '@material-ui/core/colors';
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
 
 // Create a theme instance.
-const theme = createMuiTheme({
+const theme = responsiveFontSizes(createMuiTheme({
   palette: {
-    primary: {
-      main: '#B18907',
-    },
-    secondary: {
-      main: '#19857b',
-    },
-    error: {
-      main: red.A400,
-    },
-    background: {
-      default: '#fff',
-    },
+    type: 'dark',
   },
-});
+  typography: {
+    h1: {
+      fontSize: 36,
+      fontWeight: 600,
+    },
+    h2: {
+      fontSize: 28,
+      fontWeight: 400,
+    },
+    h3: {
+      fontSize: 22,
+      fontWeight: 400,
+    },
+    fontSize: 14,
+  },
+}));
 
 export default theme;
