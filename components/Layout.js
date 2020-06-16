d5f6e0040a9bf067937f12a26e23d9c98800b41a
IpShot <zhernosek.roman@gmail.com>
2020-06-16 18:48:41 +0500

    update layout padding

--- components/Layout.js
+++ components/Layout.js
@@ -4,7 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   container: {
-    paddingTop: 50,
+    padding: theme.spacing(6, 3),
   },
 }));
 
