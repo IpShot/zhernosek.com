2a9efc93f8ad913f705be1a4351509e19fd5d67d
IpShot <zhernosek.roman@gmail.com>
2020-06-19 16:57:33 +0500

    fix email icon

--- components/Icons.js
+++ components/Icons.js
@@ -34,7 +34,7 @@ export const UpworkIcon = () => (
 export const EmailIcon = () => (
   <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 14 16" height="1.25em" width="1.25em" xmlns="http://www.w3.org/2000/svg">
     <g>
-      <path fill-rule="evenodd" d="M0 4v8c0 .55.45 1 1 1h12c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1H1c-.55 0-1 .45-1 1zm13 0L7 9 1 4h12zM1 5.5l4 3-4 3v-6zM2 12l3.5-3L7 10.5 8.5 9l3.5 3H2zm11-.5l-4-3 4-3v6z"></path>
+      <path fillRule="evenodd" d="M0 4v8c0 .55.45 1 1 1h12c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1H1c-.55 0-1 .45-1 1zm13 0L7 9 1 4h12zM1 5.5l4 3-4 3v-6zM2 12l3.5-3L7 10.5 8.5 9l3.5 3H2zm11-.5l-4-3 4-3v6z"></path>
     </g>
   </svg>
 );
