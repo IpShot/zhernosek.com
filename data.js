ba5f74e739033b231e6a86579c86bc368c88532a
IpShot <zhernosek.roman@gmail.com>
2020-06-23 21:10:30 +0500

    update portfolio projects details

--- data.js
+++ data.js
@@ -92,8 +92,9 @@ export const SKILLS = {
 export const PORTFOLIO = [{
   name: 'JobNinja',
   desc: 'Jobs marketplace in Germany',
-  details: 'Client wanted to develop web desktop client and mobile apps for \
-  search jobs by criterias and apply to them. Service was developed for Germany.',
+  details: 'Client wanted to develop a responive web client and \
+  mobile apps for searching jobs according to set criterias and \
+  applying to them. The service has been developed for Germany.',
   url: 'https://jobninja.com',
   images: ['landing.jpg', 'job search', 'job page.jpg', 'profile'],
   stack: [
@@ -102,8 +103,13 @@ export const PORTFOLIO = [{
   ],
 },{
   name: 'LightYear',
-  desc: 'Courses and leadership trainings',
-  details: '',
+  desc: 'Personal development community',
+  details: 'Client wanted to develop a web community for \
+  personal development and leadership trainings. The platform \
+  allows people to communicate and exchange knowledge via either \
+  private or group chats, and people dashboards. They are able to \
+  enroll in courses and trainings for accomplishing their goals. \
+  Users and courses can be managed by an admin.',
   url: 'https://lightyear.co',
   logo: 'logo.svg',
   imageMaxHeight: '170px',
@@ -120,10 +126,14 @@ export const PORTFOLIO = [{
 },{
   name: 'Briefly',
   desc: 'Connect brands and photographers',
-  details: 'Client wanted to create a platform for connect brands and photographs. \
-  Brand should be able to post info about required photos content, select creator, \
-  discuss project with creator, review photos, complete project and pay. \
-  Creator should be able to view project info, submit proposal, discuss project with brand, upload photos.',
+  details: 'Client wanted to create a web platform which connects \
+  brands and photographers. Brands are able to post an information \
+  about what photos content do they need, choose appropriate creator, \
+  discuss the project, review photos, select appropriate ones and pay \
+  for them. Photographers are able to view available projects, \
+  submit proposals for interested ones, discuss a project with brand, \
+  upload photos and get payouts for them. Brands, photographers, \
+  ptojects, payments and payouts can be managed by an admin.',
   images: [
     'landing.jpg', 'login', 'new project modal', 'brand start create project',
     'creator projects', 'creator project info', 'creator submit proposal',
@@ -140,11 +150,9 @@ export const PORTFOLIO = [{
   name: 'Teem',
   desc: 'The US schools platform',
   details: 'Client wanted to develop a web app that allows school staffs \
-  to find each other and communicate easily, to search any useful files/links \
-  which help them at work. Also it should has an admin side for managing schools \
-  and districts (group of schools in America). The managing includes managing users (staffs) and topics.\
-  I built almost all in the app using Meteor + React. \
-  Also I used Meteor\'s Cordova integration to wrap an app for mobile iOS and Android.',
+  to find each other and communicate easily, search any useful files/links \
+  which help them at work. The app has an admin side for managing schools \
+  and districts (group of schools in the US), users and topics.',
   images: [
     'landing', 'sign in', 'school staffs list', 'create new topic',
     'topic channel', 'notifications', 'global search', 'profile',
@@ -158,11 +166,9 @@ export const PORTFOLIO = [{
   name: 'MiID',
   desc: 'Customizable products marketplace',
   details: 'Client wanted to develop a web app that allows users \
-  to customize products from different brands (and see 3D model at real time), \
-  buy them or products customized by another users, add products to favorites, \
-  like products, following another users. I built a front-end part in React \
-  with flux architecture to get data from API. \
-  For building front-end project I had used Webpack.',
+  to customize products from different brands (and see 3D model at real time). \
+  They can buy either own created products or customized by another users, \
+  save products in favorites, like products, follow another users.',
   images: [
     'landing.jpg', 'brand page', 'customization', 'cart', 'order info',
     'shopping history', 'account', 'public profile'
@@ -192,18 +198,18 @@ export const FEEDBACKS = [{
   It was a very nice experience to work with Roman and we will be \
   surely crossing paths in the future to build something great again.',
 },{
-  project: 'Briefly',
-  owner: 'Kirk Bridger, PM',
-  feedback: 'Roman is a truly good freelancer, very flexible and able to work 60 hours a week to meet a deadline. \
-  I can read through the code he writes without any problem and so does my whole team. \
-  The code he writes is modular and scalable and respects the best practices set by the industry. \
-  I warmly recommend Roman if you need a solid frontend :)',
-},{
   project: 'Teem',
   owner: 'Kevin Jenkins, CEO',
   feedback: 'Roman is awesome. He\'s fast. But more importantly, he cares about the work he does. \
   And that makes all the difference. For example, frustrated with performance issues, he knocked them out. \
   Would highly recommend!',
+},{
+  project: 'Briefly',
+  owner: 'Kirk Bridger, PM',
+  feedback: 'Roman is a truly good freelancer, very flexible and able to work 60 hours a week to meet a deadline. \
+  I can read through the code he writes without any problem and so does my whole team. \
+  The code he writes is modular and scalable and respects the best practices set by the industry. \
+  I warmly recommend Roman if you need a solid frontend.',
 },{
   project: 'LightYear',
   owner: 'Igsaan Salie, PM',
